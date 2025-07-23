92719ee7810d879fb5bf48cfbf879e39d6be6b46
yV1ctorr <yagomelo@yahoo.com>
2025-07-23 12:58:51 -0300

    adição de componentes

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { Sidebar } from './Sidebar';
+
+export function App() {
+    return (
+        <div>
+            <Sidebar />
+        </div>
+    )
+}
---
+++ src/Sidebar.jsx
@@ -0,0 +1,5 @@
+export function Sidebar(){
+    return(
+        <h2>Sidebar</h2>
+    )
+}
