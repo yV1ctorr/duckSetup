1fe7a8f51b89c709e3bc88ac295761f277d37202
yV1ctorr <yagomelo@yahoo.com>
2025-08-10 00:50:31 -0300

    Global css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Sidebar } from './components/Sidebar';
 import { Header } from './components/Header';
-import './global.css';
+import './components/Global.css';
 
 export function App() {
     return (
